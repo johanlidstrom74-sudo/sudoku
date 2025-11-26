8176d4aa79772ec61fe45f7934611a7e0f01fe0a
johanlidstrom74-sudo <johan.lidstrom74@gmail.com>
2025-11-26 12:26:37 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "sudoku-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "index.html",
+  "manifest.webmanifest"
+  // Lägg till "icons/icon-192.png", "icons/icon-512.png" om du vill cachea ikonerna också
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
